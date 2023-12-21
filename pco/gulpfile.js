9ef3c37d54ec9e461e876a422391b7d7ace5ba6b
karlisup <karlis@upitis.com>
2023-12-21 15:51:01 +0200

    Now generating WEBP from the shell

--- pco/gulpfile.js
+++ pco/gulpfile.js
@@ -3,6 +3,10 @@ var gulp = require("gulp"),
   sass = require("gulp-sass")(require("sass")),
   twig = require("gulp-twig"),
   responsive = require("gulp-responsive"),
+  // imagemin  = require("gulp-imagemin"),
+  shell = require('gulp-shell'),
+  glob = require('glob'),
+  path = require('path'),
   browserSync = require("browser-sync").create();
 
 gulp.task("css", function () {
@@ -17,29 +21,37 @@ gulp.task("css", function () {
     .pipe(browserSync.stream());
 });
 
-gulp.task("images-benefits", function () {
+gulp.task('convert-to-webp', done => {
+  const normal = glob.sync('src/assets/benefits/normal/*.jpg');
+  const wide = glob.sync('src/assets/benefits/wide/*.jpg');
+  const folders = [normal, wide]
+  folders.forEach(folder => {
+    folder.forEach(file => {
+      const outputPath = path.join(
+        path.dirname(file),
+        path.basename(file, path.extname(file)) + '.webp'
+      );
+      shell.task([
+        `cwebp -q 95 ${file} -o ${outputPath}`
+      ])();
+    });
+  })
+  done();
+});
+
+gulp.task("resize-jpg", function () {
   return gulp
-    .src(["assets/benefits/normal/*.jpg", "assets/benefits/wide/*.jpg"]) // Include both selectors
+    .src(["src/assets/benefits/normal/*.jpg", "src/assets/benefits/wide/*.jpg"]) // Include both selectors
     .pipe(
       responsive(
         {
           "**/*.jpg": [ // Selector for normal .jpg
-            {
-              width: 356,
-            },
-            {
-              width: 712,
-              rename: { suffix: "x2" },
-            },
+            { width: 356 },
+            { width: 712, rename: { suffix: "x2" } },
           ],
           "**/*.jpg": [ // Selector for wide .jpg
-            {
-              width: 724,
-            },
-            {
-              width: 1448,
-              rename: { suffix: "x2" },
-            },
+            { width: 724 },
+            { width: 1448, rename: { suffix: "x2" } },
           ],
         },
         {
@@ -47,11 +59,37 @@ gulp.task("images-benefits", function () {
           quality: 95,
           progressive: true,
           withMetadata: false,
+          withoutEnlargement: false, // This will generate image also if it is smaller than needed image
         }
       )
     )
-    .pipe(gulp.dest(destroot + "assets/images/benefits"));
+    .pipe(gulp.dest(destroot + "assets/images/benefits"))
 });
+
+gulp.task("resize-webp", function () {
+  return gulp
+    .src(["src/assets/benefits/normal/*.webp", "src/assets/benefits/wide/*.webp"]) // Include both selectors
+    .pipe(
+      responsive(
+        {
+          "**/*.webp": [ // Selector for normal .jpg
+            { width: 356 },
+            { width: 712, rename: { suffix: "x2" } },
+          ],
+          "**/*.webp": [ // Selector for wide .jpg
+            { width: 724 },
+            { width: 1448, rename: { suffix: "x2" } },
+          ],
+        },
+        {
+          withMetadata: false,
+          withoutEnlargement: false, // This will generate image also if it is smaller than needed image
+        }
+      )
+    )
+    .pipe(gulp.dest(destroot + "assets/images/benefits"))
+});
+
 
 
 gulp.task("compile", function (done) {

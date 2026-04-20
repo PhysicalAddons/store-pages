a130721cf42eb82286b5d7427419ff05c38e00ec
Karlis Upitis <karlis@upitis.com>
2026-04-20 18:09:31 +0300

    Add physical atmosphere listing page

---
+++ pa/gulpfile.js
@@ -0,0 +1,44 @@
+var destroot = "public/";
+var gulp = require("gulp"),
+  sass = require("gulp-sass")(require("sass")),
+  twig = require("gulp-twig"),
+  browserSync = require("browser-sync").create();
+
+gulp.task("css", function () {
+  return gulp
+    .src("src/style.scss")
+    .pipe(
+      sass({
+        // outputStyle: 'compressed'
+      }).on("error", sass.logError)
+    )
+    .pipe(gulp.dest(destroot + "assets/css/"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("compile", function (done) {
+  return gulp
+    .src("src/index.twig")
+    .pipe(twig({ data: {} }))
+    .pipe(gulp.dest(destroot))
+    .pipe(browserSync.stream());
+  done();
+});
+
+gulp.task("browserSync", function () {
+  browserSync.init({
+    open: false,
+    server: {
+      baseDir: destroot,
+    },
+  });
+});
+
+
+gulp.task("watch", function () {
+  gulp.watch("./**/*.scss", gulp.series("css"));
+  gulp.watch("./**/*.twig", gulp.series("compile"));
+});
+
+gulp.task("default", gulp.parallel("browserSync", "compile", "watch", "css"));
+gulp.task("build", gulp.parallel("compile", "css"));
